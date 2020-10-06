6bc7aed23ce7b7d275bd76e392e81c10d7067d09
LouisDumont <dumont.louis21@gmail.com>
2020-10-07 01:11:56 +0200

    feat BDM: add choices to display

--- BDM_project/test_display.js
+++ BDM_project/test_display.js
@@ -1,23 +1,37 @@
-// alert("Test")
-
-// var requestURL = "https://LouisDumont.github.io/BDM_project/Content/test.json";
 var requestURL = "https://LouisDumont.github.io/BDM_project/Content/first_chapter.json";
 var request = new XMLHttpRequest();
+
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-request.onload = function() {
-    var content = request.response;
-    node_text = content[0]["description"]
-    alert(node_text); // ["id"]
-    // var test = prompt("Quel contenu afficher?");
-    document.getElementById("node_text").innerText = node_text;
+
+// request.onload = function() {
+//     var content = request.response;
+//     node_text = content[0]["description"];
+//     choices = content[0]["choices"];
+//     document.getElementById("node_text").innerText = node_text;
+// }
+
+function affect_content(some_request){
+  var content = some_request.response
+  return(content)
+}
+
+function print_content(content){
+  node_text = content[0]["description"];
+  choices = content[0]["choices"];
+
+  document.getElementById("node_text").innerText = node_text;
+
+  for (i=0, c=choices.length; i<c; i++){
+    var new_choice_text = document.createElement('p');
+    new_choice_text.innerText = choices[i]["description"];
+    var new_choice = document.createElement('li');
+    new_choice.appendChild(new_choice_text);
+    document.getElementById("choice_list").appendChild(new_choice)
   }
+}
 
-/*
-var age = parseInt(prompt("Quel est votre âge?"))
-
-if (age<0){
-  alert("Veuillez entrer un âge valide")
-} else if (age)
-*/
+request.onload = function(){
+  print_content(affect_content(request))
+}
