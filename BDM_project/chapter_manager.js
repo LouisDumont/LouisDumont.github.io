68d53148d15d1b191bd4b9222a5b58041cd95310
LouisDumont <dumont.louis21@gmail.com>
2021-01-11 12:56:30 +0100

    test fix: fixing index value in make_nodes_dict

--- BDM_project/chapter_manager.js
+++ BDM_project/chapter_manager.js
@@ -14,10 +14,10 @@ function ChapterManager(init_state){
         // Returns a dict mapping from node_ids to idx in the content array
         var dict = {};
         // var idx = 1;  // I started at 1 for historical reasons (fear of introducing a bug), should be 0
-        for (var i=1, c=content.length+1; i<c; i++){
+        for (var i=0, c=content.length; i<c; i++){
             var node = content[i];
             //var idx = nodeId_to_int_parser(node.id);
-            dict[node.id] = i;
+            dict[node.id] = i+1;
             //idx += 1;
         }
         return dict;
